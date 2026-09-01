b6daf8f7bcdff98af9c4376410ae495b80403f9b
d34dscene <d34dscene@mizuchi.dev>
2026-09-01 21:28:30 +0200

    chore(gen): sort generated imports

--- internal/gen/nokku/v1/util.pb.go
+++ internal/gen/nokku/v1/util.pb.go
@@ -7,15 +7,16 @@
 package nokkuv1
 
 import (
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
+
 	_ "buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	structpb "google.golang.org/protobuf/types/known/structpb"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
 )
 
 const (
