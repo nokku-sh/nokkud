c8758ff5e850fb8def054fed153a8cd7a70d794b
d34dscene <d34dscene@mizuchi.dev>
2026-09-11 16:01:51 +0200

    refactor(gen/nokku): renumbered daemon config proto fields
    - Moved max_channels, conn_rate, conn_rate_burst, drop_retired_ca, and client_alive_interval to new field numbers.
    - Ordered generated accessor methods to match the new field layout.

--- internal/gen/nokku/v1/daemon.pb.go
+++ internal/gen/nokku/v1/daemon.pb.go
@@ -196,18 +196,13 @@ type DaemonConfig struct {
 	MaxSessionsPerUser   *int32                 `protobuf:"varint,7,opt,name=max_sessions_per_user,json=maxSessionsPerUser" json:"max_sessions_per_user,omitempty"`
 	MaxConnections       *int32                 `protobuf:"varint,8,opt,name=max_connections,json=maxConnections" json:"max_connections,omitempty"`
 	MaxStartups          *int32                 `protobuf:"varint,9,opt,name=max_startups,json=maxStartups" json:"max_startups,omitempty"`
-	ConnRate             *int32                 `protobuf:"varint,10,opt,name=conn_rate,json=connRate" json:"conn_rate,omitempty"`
-	ConnRateBurst        *int32                 `protobuf:"varint,11,opt,name=conn_rate_burst,json=connRateBurst" json:"conn_rate_burst,omitempty"`
-	ClientAliveInterval  *durationpb.Duration   `protobuf:"bytes,12,opt,name=client_alive_interval,json=clientAliveInterval" json:"client_alive_interval,omitempty"`
-	// Channels a single connection may hold open across all types (sessions,
-	// port forwards, agent forwards). Bounds resource use per authenticated
-	// connection. Zero disables the cap.
-	MaxChannels *int32 `protobuf:"varint,13,opt,name=max_channels,json=maxChannels" json:"max_channels,omitempty"`
-	// Stop trusting a rotated-out CA immediately instead of keeping it valid
-	// for the daemon's grace window.
-	DropRetiredCa *bool `protobuf:"varint,14,opt,name=drop_retired_ca,json=dropRetiredCa" json:"drop_retired_ca,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
+	MaxChannels          *int32                 `protobuf:"varint,10,opt,name=max_channels,json=maxChannels" json:"max_channels,omitempty"`
+	ConnRate             *int32                 `protobuf:"varint,11,opt,name=conn_rate,json=connRate" json:"conn_rate,omitempty"`
+	ConnRateBurst        *int32                 `protobuf:"varint,12,opt,name=conn_rate_burst,json=connRateBurst" json:"conn_rate_burst,omitempty"`
+	DropRetiredCa        *bool                  `protobuf:"varint,13,opt,name=drop_retired_ca,json=dropRetiredCa" json:"drop_retired_ca,omitempty"`
+	ClientAliveInterval  *durationpb.Duration   `protobuf:"bytes,14,opt,name=client_alive_interval,json=clientAliveInterval" json:"client_alive_interval,omitempty"`
+	unknownFields        protoimpl.UnknownFields
+	sizeCache            protoimpl.SizeCache
 }
 
 func (x *DaemonConfig) Reset() {
@@ -303,6 +298,13 @@ func (x *DaemonConfig) GetMaxStartups() int32 {
 	return 0
 }
 
+func (x *DaemonConfig) GetMaxChannels() int32 {
+	if x != nil && x.MaxChannels != nil {
+		return *x.MaxChannels
+	}
+	return 0
+}
+
 func (x *DaemonConfig) GetConnRate() int32 {
 	if x != nil && x.ConnRate != nil {
 		return *x.ConnRate
@@ -317,25 +319,18 @@ func (x *DaemonConfig) GetConnRateBurst() int32 {
 	return 0
 }
 
-func (x *DaemonConfig) GetClientAliveInterval() *durationpb.Duration {
-	if x != nil {
-		return x.ClientAliveInterval
-	}
-	return nil
-}
-
-func (x *DaemonConfig) GetMaxChannels() int32 {
-	if x != nil && x.MaxChannels != nil {
-		return *x.MaxChannels
-	}
-	return 0
-}
-
 func (x *DaemonConfig) GetDropRetiredCa() bool {
 	if x != nil && x.DropRetiredCa != nil {
 		return *x.DropRetiredCa
 	}
 	return false
+}
+
+func (x *DaemonConfig) GetClientAliveInterval() *durationpb.Duration {
+	if x != nil {
+		return x.ClientAliveInterval
+	}
+	return nil
 }
 
 type GetDaemonRequest struct {
@@ -2651,13 +2646,13 @@ const file_nokku_v1_daemon_proto_rawDesc = "" +
 	"\fmax_sessions\x18\x06 \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\vmaxSessions\x12:\n" +
 	"\x15max_sessions_per_user\x18\a \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\x12maxSessionsPerUser\x120\n" +
 	"\x0fmax_connections\x18\b \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\x0emaxConnections\x12*\n" +
-	"\fmax_startups\x18\t \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\vmaxStartups\x12$\n" +
-	"\tconn_rate\x18\n" +
-	" \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\bconnRate\x12/\n" +
-	"\x0fconn_rate_burst\x18\v \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\rconnRateBurst\x12M\n" +
-	"\x15client_alive_interval\x18\f \x01(\v2\x19.google.protobuf.DurationR\x13clientAliveInterval\x12*\n" +
-	"\fmax_channels\x18\r \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\vmaxChannels\x12&\n" +
-	"\x0fdrop_retired_ca\x18\x0e \x01(\bR\rdropRetiredCa\"Y\n" +
+	"\fmax_startups\x18\t \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\vmaxStartups\x12*\n" +
+	"\fmax_channels\x18\n" +
+	" \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\vmaxChannels\x12$\n" +
+	"\tconn_rate\x18\v \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\bconnRate\x12/\n" +
+	"\x0fconn_rate_burst\x18\f \x01(\x05B\a\xbaH\x04\x1a\x02(\x00R\rconnRateBurst\x12&\n" +
+	"\x0fdrop_retired_ca\x18\r \x01(\bR\rdropRetiredCa\x12M\n" +
+	"\x15client_alive_interval\x18\x0e \x01(\v2\x19.google.protobuf.DurationR\x13clientAliveInterval\"Y\n" +
 	"\x10GetDaemonRequest\x12+\n" +
 	"\fworkspace_id\x18\x01 \x01(\tB\b\xbaH\x05r\x03\xb0\x01\x01R\vworkspaceId\x12\x18\n" +
 	"\x02id\x18\x02 \x01(\tB\b\xbaH\x05r\x03\xb0\x01\x01R\x02id\"=\n" +
